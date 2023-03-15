cbca0478a583da55d0572bf28330cff731ff2b07
Mitzter <79134141+Mitzter@users.noreply.github.com>
2023-03-15 19:38:15 +0200

    Update SoftUniContext.cs

--- SoftUni/SoftUni/Data/SoftUniContext.cs
+++ SoftUni/SoftUni/Data/SoftUniContext.cs
@@ -30,7 +30,7 @@ namespace SoftUni.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-I1LEVMQ\\AA;Database=SoftUni;Integrated Security=True;Encrypt=False;");
+                optionsBuilder.UseSqlServer("Server=.;Database=SoftUni;Integrated Security=True;Encrypt=False;");
             }
         }
 
